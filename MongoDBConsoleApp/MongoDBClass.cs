359b91c86e5066e2dd3ef4b47f866de483194d05
Волков Виктор <volkov.vik1@gmail.com>
2019-11-11 22:13:20 +0300

    Add project files.

---
+++ MongoDBConsoleApp/MongoDBClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+
+namespace MongoDBConsoleApp {
+    class MongoDBClass {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly MongoClient _client;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IMongoCollection< BsonDocument > _collection;
+
+
+        public MongoDBClass() {
+            //string         connectionString = "mongodb://localhost:27017";
+            //MongoClient    client           = new MongoClient(connectionString);
+            //IMongoDatabase database         = client.GetDatabase("test");
+
+            var connectionString = ConfigurationManager.ConnectionStrings [ "MongoDb" ].ConnectionString;
+            _client     = new MongoClient( connectionString );
+            _database   = _client.GetDatabase( "sample_training" );
+            _collection = _database.GetCollection< BsonDocument >( "grades" );
+        }
+
+        /// <summary>
+        /// Получить список баз данных сервера
+        /// </summary>
+        /// <returns></returns>
+        public List< BsonDocument > GetDatabasesList() => _client.ListDatabases().ToList();
+
+        /// <summary>
+        /// Получить список баз данных сервера
+        /// </summary>
+        /// <returns></returns>
+        public List< BsonDocument > GetCollectionsList( string name ) =>
+            _client.GetDatabase( name ).ListCollections().ToList();
+
+
+        public void Create() {
+            var document = new BsonDocument {
+                                                { "student_id", 10000 }, {
+                                                    "scores",
+                                                    new BsonArray {
+                                                                      new BsonDocument {
+                                                                                           { "type", "exam" }, {
+                                                                                               "score",
+                                                                                               88.12334193287023
+                                                                                           }
+                                                                                       },
+                                                                      new BsonDocument {
+                                                                                           { "type", "quiz" }, {
+                                                                                               "score",
+                                                                                               74.92381029342834
+                                                                                           }
+                                                                                       },
+                                                                      new BsonDocument {
+                                                                                           { "type", "homework" }, {
+                                                                                               "score",
+                                                                                               89.97929384290324
+                                                                                           }
+                                                                                       },
+                                                                      new BsonDocument {
+                                                                                           { "type", "homework" }, {
+                                                                                               "score",
+                                                                                               82.12931030513218
+                                                                                           }
+                                                                                       }
+                                                                  }
+                                                },
+                                                { "class_id", 480 }
+                                            };
+            _collection.InsertOne( document );
+        }
+
+        public List<BsonDocument> ReadAll() => _collection.Find( new BsonDocument() ).ToList();
+
+        public BsonDocument Read() => _collection.Find( new BsonDocument() ).FirstOrDefault();
+
+    }
+}
---
+++ MongoDBConsoleApp/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+
+namespace MongoDBConsoleApp {
+    class Program {
+        static void Main( string [] args ) {
+
+            // ReSharper disable once InconsistentNaming
+            var mongoDB = new MongoDBClass();
+
+
+            Console.WriteLine( "The list of databases on this server is: " );
+            foreach ( var db in mongoDB.GetDatabasesList() ) {
+                Console.WriteLine( db [ "name" ] );
+                foreach ( var dbCol in mongoDB.GetCollectionsList( db [ "name" ].ToString() ) ) {
+                    Console.WriteLine( "    " + dbCol [ "name" ] );
+                }
+            }
+
+            mongoDB.Create();
+            var list = mongoDB.ReadAll();
+            foreach ( var item in list ) {
+                Console.WriteLine( item.ToString() );
+            }
+            
+
+
+
+
+            ////string         connectionString = "mongodb://localhost:27017";
+            ////MongoClient    client           = new MongoClient(connectionString);
+            ////IMongoDatabase database         = client.GetDatabase("test");
+
+            //string con    = ConfigurationManager.ConnectionStrings [ "MongoDb" ].ConnectionString;
+            //var    client = new MongoClient( con );
+            //// GetDatabaseNames( client ).GetAwaiter();
+            //// GetCollectionsNames( client ).GetAwaiter();
+
+            //// Список баз данных
+            //var dbList = client.ListDatabases().ToList();
+            //Console.WriteLine( "The list of databases on this server is: " );
+            //foreach ( var db in dbList ) {
+            //    Console.WriteLine( db [ "name" ] );
+            //}
+
+
+
+            //GetDatabaseNames( client );
+
+
+
+            //IMongoDatabase                   database = client.GetDatabase( "test" );
+            //IMongoCollection< BsonDocument > col      = database.GetCollection< BsonDocument >( "users" );
+
+            //// Создание документа
+            //BsonDocument doc = new BsonDocument { { "name", "Bill" } };
+            //Console.WriteLine( doc );
+            //Console.WriteLine( doc [ "name" ] );
+            //// изменим поле name
+            //doc [ "name" ] = "Tom";
+
+            //Console.WriteLine( doc.GetValue( "name" ) );
+
+            Console.ReadLine();
+        }
+
+        //    private static async Task GetDatabaseNamesAsync( MongoClient client ) {
+        //        using( var cursor = await client.ListDatabasesAsync() ) {
+        //            var databaseDocuments = await cursor.ToListAsync();
+        //            foreach( var databaseDocument in databaseDocuments ) {
+        //                Console.WriteLine( databaseDocument["name"] );
+        //            }
+        //        }
+        //    }
+
+        //    private static List< string > GetDatabaseNames( IMongoClient client ) {
+        //        var list = new List< string >();
+        //        using ( var cursor = client.ListDatabases() ) {
+        //            var databaseDocuments = cursor.ToList();
+        //            foreach ( var databaseDocument in databaseDocuments ) {
+        //                list.Add( databaseDocument [ "name" ].ToString() );
+        //            }
+        //        }
+        //        return list;
+        //    }
+
+
+        //    private static async Task GetCollectionsNames( MongoClient client ) {
+        //        using ( var cursor = await client.ListDatabasesAsync() ) {
+        //            var dbs = await cursor.ToListAsync();
+        //            foreach ( var db in dbs ) {
+        //                Console.WriteLine( "В базе данных {0} имеются следующие коллекции:", db [ "name" ] );
+
+        //                IMongoDatabase database = client.GetDatabase( db [ "name" ].ToString() );
+
+        //                using ( var collCursor = await database.ListCollectionsAsync() ) {
+        //                    var colls = await collCursor.ToListAsync();
+        //                    foreach ( var col in colls ) {
+        //                        Console.WriteLine( col [ "name" ] );
+        //                    }
+        //                }
+        //                Console.WriteLine();
+        //            }
+
+        //        }
+        //    }
+
+        //    /// <summary>
+        //    /// Создание документа
+        //    /// </summary>
+        //    private static void Create() {
+        //        BsonDocument doc = new BsonDocument { {"name","Bill"}};
+        //        BsonElement bel = new BsonElement("name", "Ted");
+        //        doc.Add( bel );
+        //        doc.Add( "countries", new BsonArray( new[] { "Бразилия", "Аргентина", "Германия", "Нидерланды" } ) );
+        //        doc.Add( "finished",  new BsonBoolean( true ) );
+        //        BsonDocument doc1 = new BsonDocument {
+        //                               {"name","Bill"},
+        //                               {"surname", "Gates"},
+        //                               {"age", new BsonInt32(48)},
+        //                               { "company",
+        //                                   new BsonDocument{
+        //                                                       {"name" , "microsoft"},
+        //                                                       {"year", new BsonInt32(1974)},
+        //                                                       {"price", new BsonInt32(300000)},
+        //                                                   }
+        //                               }
+        //                           };
+
+        //    }
+        //}
+    }
+
+}
